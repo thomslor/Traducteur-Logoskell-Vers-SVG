a9dbc9fad83dee263467b89f50138891ab67ed44
T-lorrain <72496171+T-lorrain@users.noreply.github.com>
2020-11-23 20:57:22 +0100

    Fonction leture
    Permet de lire les instructions

--- projet.hs
+++ projet.hs
@@ -2,3 +2,10 @@ import Prelude hiding (Left, Right)
 
 data Instruction = Forward Int | Left Int | Right Int | Repeat Int [Instruction]
   deriving (Read, Show)
+
+lecture :: IO [Instruction]
+lecture = do
+  putStrLn "Entrez Instructions : "
+  line <- getLine
+  let lesInstruction = (read line :: [Instruction])
+  return (lesInstruction)

c5c4dd6bde2d3cc8d925fe8a0ce1e898d0214e23
T-lorrain <72496171+T-lorrain@users.noreply.github.com>
2020-12-03 17:33:46 +0100

    Début logoskell2svg

--- projet.hs
+++ projet.hs
@@ -9,17 +9,19 @@ type Dirx = Int
 type Diry = Int
 data Crayon = DonneCrayon {position::[(Posx,Posy)], direction::[(Dirx,Diry)]}
 
-data Programme = DonneProgramme (IO [Instruction])
+data Programme = DonneProgramme [Instruction]
   deriving (Read, Show)
 
-
+logoskell2svg :: Programme -> Crayon -> [String] -> (Crayon, [String]) --[String] correspond à une ligne de SVG
+--idée : Prendre programme, faire une ligne à partir de la première instruction, puis enlever la première instruction de Programme et récursion avec cette nouvelle liste
+logoskell2svg progs
 
 
 
 main = do
-  let cray = DonneCrayon [(100,100)] [(0,0)]
+  --let cray = DonneCrayon [(100,100)] [(0,0)]
   putStrLn "Entrez Instructions : "
   line <- getLine
   let lesInstructions = (read line :: [Instruction])
   let progs = DonneProgramme lesInstructions
-  return progs
+  logoskell2svg progs

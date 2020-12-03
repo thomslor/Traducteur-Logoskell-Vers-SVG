4128e23ffe9df4a0afed49abfba51bd632c046bf
T-lorrain <72496171+T-lorrain@users.noreply.github.com>
2020-12-03 16:57:22 +0100

    Bases saines pour début logoskell2svg

--- projet.hs
+++ projet.hs
@@ -3,15 +3,23 @@ import Prelude hiding (Left, Right)
 data Instruction = Forward Int | Left Int | Right Int | Repeat Int [Instruction]
   deriving (Read, Show)
 
-lecture :: IO [Instruction]
-lecture = do
+type Posx = Int
+type Posy = Int
+type Dirx = Int
+type Diry = Int
+data Crayon = DonneCrayon {position::[(Posx,Posy)], direction::[(Dirx,Diry)]}
+
+data Programme = DonneProgramme (IO [Instruction])
+  deriving (Read, Show)
+
+
+
+
+
+main = do
+  let cray = DonneCrayon [(100,100)] [(0,0)]
   putStrLn "Entrez Instructions : "
   line <- getLine
   let lesInstructions = (read line :: [Instruction])
-  return (lesInstructions)
-
-type posx = Int
-type posy = Int
-type dirx = Int
-type diry = Int
-data Crayon :: DonneCrayon {position :: [(posx,posy)], direction :: [(dirx,diry)]}
+  let progs = DonneProgramme lesInstructions
+  return progs

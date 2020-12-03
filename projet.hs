70688e3b76c44f2f77d7bed59c21ceadd69cd4cb
Estelle <esmonier01@gmail.com>
2020-12-03 09:12:53 +0100

    data crayon

--- projet.hs
+++ projet.hs
@@ -7,5 +7,11 @@ lecture :: IO [Instruction]
 lecture = do
   putStrLn "Entrez Instructions : "
   line <- getLine
-  let lesInstruction = (read line :: [Instruction])
-  return (lesInstruction)
+  let lesInstructions = (read line :: [Instruction])
+  return (lesInstructions)
+
+type posx = Int
+type posy = Int
+type dirx = Int
+type diry = Int
+data Crayon :: DonneCrayon {position :: [(posx,posy)], direction :: [(dirx,diry)]}

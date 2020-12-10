8339213d6e12543d5e6122a4a89cab5e9472e3d5
T-lorrain <72496171+T-lorrain@users.noreply.github.com>
2020-12-10 22:05:00 +0100

    Logoskell2Svg fonctionnel (pb de valeur)
    Sah ca fait plaisir d'écrire ca. Par contre oui pb avec les valeurs, il va à l'envers

--- projet.hs
+++ projet.hs
@@ -1,49 +1,34 @@
 import Prelude hiding (Left, Right)
 
-data Instruction = Forward Int | Left Int | Right Int | Repeat Int [Instruction]
+data Instruction = Forward Float | Left Float | Right Float | Repeat Int [Instruction]
   deriving (Read, Show)
 
-data Crayon = DonneCrayon {positionX::Int, positionY::Int, angle::Int}
+data Crayon = DonneCrayon {positionX::Float, positionY::Float, angle::Float}
   deriving (Read, Show)
 
-data Programme = DonneProgramme {instructions::[Instruction]}
-  deriving (Read, Show)
+type Programme = [Instruction]
 
 
-logoskell2svg :: Programme -> Crayon -> [String]
+logoskell2svg :: Programme -> Crayon -> [String] -> (Crayon, [String])
 --ATTENTION : La fonction prend en entrée Programme mais aussi Crayon
---idée : Prendre programme, faire une ligne à partir de la première instruction, puis enlever la première instruction de Programme et récursion avec cette nouvelle liste et un nouveau crayon
 -- c = Crayon
-logoskell2svg [] c = []
-logoskell2svg (Forward a) c --PROBLEME : On arrive pas à mettre une instruction en paramètre
-  | angle c == 0 = ("<line x1=" ++ (show (positionX c)) ++ " y1=" ++ (show (positionY c)) ++ " x2=" ++ (show ((positionX c) + a)) ++ " y2=" ++ (show (positionY c)))
-  | angle c == 90 || angle c == -270 = ("<line x1=" ++ (show (positionX c)) ++ " y1=" ++ (show (positionY c)) ++ " x2=" ++ (show (positionX c)) ++ " y2=" ++ (show (positionY c+a)))
-  | angle c == 180 || angle c == -180 = ("<line x1=" ++ (show (positionX c)) ++ " y1=" ++ (show (positionY c)) ++ " x2=" ++ (show (positionX c-a)) ++ " y2=" ++ (show (positionY c)))
-  | angle c == 270 || angle c == -90 = ("<line x1=" ++ (show (positionX c)) ++ " y1=" ++ (show (positionY c)) ++ " x2=" ++ (show (positionX c)) ++ " y2=" ++ (show (positionY c-a)))
+logoskell2svg [] c res = (c, res)
+logoskell2svg (x:xs) c res = case x of
+  Forward i -> logoskell2svg xs (DonneCrayon (i * cos agl) (i * sin agl) (angle c)) (res ++ [("<line x1=" ++ (show (positionX c)) ++ " y1=" ++ (show (positionY c)) ++ " x2=" ++ (show (i * cos agl)) ++ " y2=" ++ (show (i * sin agl)))])
+  Left i -> logoskell2svg xs (DonneCrayon (positionX c) (positionY c) (((angle c)+i))) res
+  Right i -> logoskell2svg xs (DonneCrayon (positionX c) (positionY c) (((angle c)-i))) res
+  Repeat i j -> logoskell2svg ((take ((length j) * i)(cycle j)) ++ xs) c res
+  where
+    agl = (angle c)/pi
 
-logoskell2svg (x:xs) c
-  | x == (Forward a) && angle c == 0 = logoskell2svg (xs) DonneCrayon (positionX+a) positionY angle
-  | x == (Forward a) && angle c == 90 || angle c == -270 = logoskell2svg (xs) DonneCrayon positionX (positionY+a) angle
-  | x == (Forward a) && angle c == 180 || angle c == -180 = logoskell2svg (xs) DonneCrayon (positionX-a) positionY angle
-  | x == (Forward a) && angle c == 270 || angle c == -90 = logoskell2svg (xs) DonneCrayon positionX (positionY-a) angle
-  | x == (Left a) = logoskell2svg (xs) DonneCrayon positionX positionY ((angle + a) `mod` 360)
-  | x == (Right a) = logoskell2svg (xs) DonneCrayon positionX positionY ((angle - a) `mod` 360)
-
-
-repeat :: Programme -> Programme
-repeat [] = []
-repeat (x:xs)
-  | x == Repeat a [b] = b:xs
-  | otherwise = repeat xs
 
 main = do
-  let x = 100
-  let y = 100
-  let cray = DonneCrayon x y 0
+  let x = 100.0
+  let y = 100.0
+  let cray = DonneCrayon x y 0.0
   putStrLn "Entrez Instructions : "
   line <- getLine
-  let lesInstructions = (read line :: Instruction)
-  return lesInstructions
-  --logoskell2svg (lesInstructions) (cray)
-  --let prog = DonneProgramme lesInstructions
-  --return prog
+  let lesInstructions = (read line :: [Instruction])
+  let prog = lesInstructions
+  --logoskell2svg prog cray []
+  return prog
